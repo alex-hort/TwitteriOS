29c0f8842fff21dd72347c0d76e22352ee5e035b
Alexis Horteales <ahortealese001@alumno.uaemex.mx>
2025-12-20 19:57:45 -0600

    2.Adding tweet timestamp to feed view in view model

--- Twitter/ViewModel/TweetViewModel.swift
+++ Twitter/ViewModel/TweetViewModel.swift
@@ -18,13 +18,25 @@ struct TweetViewModel{
         return tweet.user.profileImageUrl
     }
     
+    var timestamp: String{
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.second, .minute, .hour, .weekOfMonth]
+        formatter.maximumUnitCount = 1
+        formatter.unitsStyle = .abbreviated
+        let now = Date()
+        return formatter.string(from: tweet.timestamp, to: now) ?? ""
+    }
+    
+    
     var userInfoText: NSAttributedString{
         let title = NSMutableAttributedString(string: user.fullname,
                                               attributes: [.font: UIFont(name: "HelveticaNeue-Medium", size: 14) ?? ""])
         title.append(NSAttributedString(string: " @\(user.username)",
                                         attributes: [.font: UIFont(name: "AvenirNext-Regular", size: 14) ?? "", .foregroundColor: UIColor.lightGray
-                                        ]))
+                                                    ]))
         
+        title.append(NSAttributedString(string: " • \(timestamp)",
+                                        attributes: [.font: UIFont(name: "HelveticaNeue-Light", size: 12) ?? "", .foregroundColor: UIColor.lightGray]))
         return title
     }
     
