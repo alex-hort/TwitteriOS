c71f6086ea4f44ac2079a8b52a533f2a2b716f3a
Alexis Horteales <ahortealese001@alumno.uaemex.mx>
2025-12-22 12:49:05 -0600

    Create UserCell view to show user by search page ui && add fech func

--- Twitter/API/UserService.swift
+++ Twitter/API/UserService.swift
@@ -14,8 +14,6 @@ struct UserService {
     static let shared = UserService()
     
     func fetchUser(uid: String,completion: @escaping(User) -> Void) {
-    
-        
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
             
@@ -24,6 +22,17 @@ struct UserService {
             DispatchQueue.main.async {
                 completion(user)
             }
+        }
+    }
+    
+    func fetchUsers(completion: @escaping([User]) -> Void) {
+        var users = [User]()
+        REF_USERS.observe(.childAdded) { snapshot in
+            let uid = snapshot.key
+            guard let dictionary = snapshot.value as? [String: AnyObject] else {return}
+            let user = User(uid: uid, dictionary: dictionary)
+            users.append(user)
+            completion(users)
         }
     }
 }
--- Twitter/Controller/ExploreController.swift
+++ Twitter/Controller/ExploreController.swift
@@ -7,9 +7,29 @@
 
 import UIKit
 
-class ExploreController: UIViewController{
+private let reuseIdentifier = "UserCell"
+
+class ExploreController: UITableViewController{
     
     // MARK: - Properties
+    
+    private var users = [User](){
+        didSet{tableView.reloadData()}
+    }
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private lazy var filteredUsers = [User](){
+        didSet{tableView.reloadData()}
+    }
+    
+    private var isSearchMode: Bool{
+        return searchController.isActive &&
+        !searchController.searchBar.text!.isEmpty
+    }
+    
+  
+    
     
     
     // MARK: - Lifecycle
@@ -20,7 +40,23 @@ class ExploreController: UIViewController{
         super.viewDidLoad()
 
         configureUI()
+        fetchUsers()
+        configureSearchController()
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    //MARK: API
+    
+    func fetchUsers(){
+        UserService.shared.fetchUsers { users in
+            self.users = users
+        }
     }
  
     
@@ -28,8 +64,52 @@ class ExploreController: UIViewController{
     func configureUI(){
         view.backgroundColor = .systemBackground
         navigationItem.title = "Explore"
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
         
+    }
+    
+    func configureSearchController(){
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
     }
 }
 
 
+//MARK: UITableViewDelegate/DS
+extension ExploreController{
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isSearchMode ? filteredUsers.count : users.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        cell.user = user
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        let controller = ProfileController(user: user)
+        navigationController?.pushViewController(controller, animated: true)
+    }
+}
+
+//MARK: UISearchResultsUpdating
+extension ExploreController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let searchText = searchController.searchBar.text?.lowercased() else {return}
+       
+        filteredUsers = users.filter({$0.username.contains(searchText)})
+    }
+    
+    
+}
